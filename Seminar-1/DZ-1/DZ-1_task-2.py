9fbf8399ddf492024e4cc2ff0da9b07eab1ea6b4
“yurafast” <yurafm@gmail.com>
2022-08-23 18:12:54 +0300

    2-i commit

--- Seminar-1/DZ-1/DZ-1_task-2.py
+++ Seminar-1/DZ-1/DZ-1_task-2.py
@@ -1,5 +1,9 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
+# для всех значений предикат.
+
+
 print('x y z ответ')
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            print(x, y, z, )
+            print(x, y, z, not(x or y or z) == (not(x)) and (not(y)) and (not (z)))
